ac2c1196f493124fa84b9908a5a0cedc3ef2fece
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-01-31 20:40:10 +0200

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+// Add new routes
+import CourseLanding from './pages/CourseLanding';
+import ReferralDashboard from './pages/ReferralDashboard';
+
+// Add to routes
+<Route path="/course" element={<CourseLanding />} />
+<Route path="/referral" element={<ReferralDashboard />} />
